aca42a0965e5bb3d60e4326ea94678addbd9453b
aleksey <lekz112@gmail.com>
2019-04-27 20:17:09 +0200

    Remove local dd-trace

--- src/dd-trace/src/native/metrics/utils.cpp
+++
@@ -1,9 +0,0 @@
-#include <nan.h>
-
-#include "utils.hpp"
-
-namespace datadog {
-  std::string to_string(v8::Local<v8::Value> handle) {
-    return *Nan::Utf8String(handle);
-  }
-}
--- src/dd-trace/src/native/metrics/utils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-#include <v8.h>
-
-namespace datadog {
-  std::string to_string(v8::Local<v8::Value> handle);
-}
