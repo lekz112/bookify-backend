5e3ad61d4971748d2c0a923a4bdf1dbd2efbf873
aleksey <lekz112@gmail.com>
2019-04-27 17:33:23 +0200

    Add dd-trace to source. Add aditional logging.

--- src/datadog.ts
+++ src/datadog.ts
@@ -1,16 +1,18 @@
-import tracer from "dd-trace";
+import tracer from "./dd-trace";
 import request from 'request';
 
 if (process.env.NODE_ENV == 'production') {
     // For amazon ECS we need to find their private address to ship logs to
     // https://docs.datadoghq.com/integrations/amazon_ecs/?tab=python#application-container
     request('http://169.254.169.254/latest/meta-data/local-ipv4', function (error, resp, body) {        
-        tracer.init({ hostname: body, debug: true })
+        tracer.init({ hostname: body })
         tracer.use("pg");
         tracer.use("graphql");
         tracer.use("koa");
 
     });                
+} else {
+    tracer.init();
 }
 
 export default tracer;
