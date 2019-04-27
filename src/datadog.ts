5140551d5953e3488a86d84a8880bafc6708c552
aleksey <lekz112@gmail.com>
2019-04-27 16:59:29 +0200

    Enable datadog for production builds only

--- src/datadog.ts
+++ src/datadog.ts
@@ -4,21 +4,13 @@ import request from 'request';
 if (process.env.NODE_ENV == 'production') {
     // For amazon ECS we need to find their private address to ship logs to
     // https://docs.datadoghq.com/integrations/amazon_ecs/?tab=python#application-container
-    if (process.env.TRACE_AGENT_HOST) {
-        tracer.init({ hostname: process.env.TRACE_AGENT_HOST, debug: true })    
-    } else {
-        request('http://169.254.169.254/latest/meta-data/local-ipv4', function (error, resp, body) {        
-            tracer.init({ hostname: body, debug: true })
-        });
-    }        
-    
-} else {
-    // Otherwise, assume that we are running in locally
-    tracer.init({ debug: true })
-} 
+    request('http://169.254.169.254/latest/meta-data/local-ipv4', function (error, resp, body) {        
+        tracer.init({ hostname: body, debug: true })
+        tracer.use("pg");
+        tracer.use("graphql");
+        tracer.use("koa");
 
-tracer.use("pg");
-tracer.use("graphql");
-tracer.use("koa");
+    });                
+}
 
 export default tracer;
